4b772a944999b094bf06ea3400c81c21f2122438
ori mamo <mori8056@gmail.com>
2022-01-11 22:22:10 +0200

    11.01.2022

--- src/Product.java
+++ src/Product.java
@@ -3,4 +3,33 @@ public class Product {
     private double price;
     private double discount;
 
+    public Product(String description, double price, double discount) {
+        this.description = description;
+        this.price = price;
+        this.discount = discount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setDiscount(double discount) {
+        this.discount = discount;
+    }
 }
